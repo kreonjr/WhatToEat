1ab17a9f0f644268710dec19be568f83ccc421d2
kreonjr <ccreonopoulos@gmail.com>
2013-11-19 13:18:27 -0600

    Initial Push

--- IngredientCell.h
+++ IngredientCell.h
@@ -1,2 +1,19 @@
 //
 //  IngredientCell.h
+//  WhatToEat
+//
+//  Created by Creonopoulos Creon on 4/13/13.
+//  Copyright (c) 2013 apt.gr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IngredientCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *IngredImage;
+
+@property (weak, nonatomic) IBOutlet UILabel *Ingredient;
+
+@property (weak, nonatomic) IBOutlet UILabel *Subtitle;
+
+@end
